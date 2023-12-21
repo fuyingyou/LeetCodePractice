5f5bd8ab47091d6ba1a9e0f4ba8c788cde262fc8
付十一 <694256602@qq.com>
2023-12-21 11:13:37 +0800

    2023.12.21 jdk版本更新8-17

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-// 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
-// 然后按 Enter 键。现在，您可以在代码中看到空格字符。
 public class Main {
     public static void main(String[] args) {
-        // 当文本光标位于高亮显示的文本处时按 Alt+Enter，
-        // 可查看 IntelliJ IDEA 对于如何修正该问题的建议。
-        System.out.printf("Hello and welcome!");
-
-        // 按 Shift+F10 或点击装订区域中的绿色箭头按钮以运行脚本。
-        for (int i = 1; i <= 5; i++) {
-
-            // 按 Shift+F9 开始调试代码。我们已为您设置了一个断点，
-            // 但您始终可以通过按 Ctrl+F8 添加更多断点。
-            System.out.println("i = " + i);
+        String s = "55AA";
+        int len = s.length();
+        byte[] bytes = new byte[len / 2];
+        for (int i = 0; i < s.length(); i += 2) {
+            bytes[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
         }
+        for (byte b : bytes) {
+            System.out.println(b);
+        }
+        System.out.println(0x55);
+        System.out.println(0xAA);
     }
 }
+
