c83304893d370354ef695bde4bdb6340f24d618d
付十一 <694256602@qq.com>
2024-03-20 11:02:59 +0800

    2024.3.20 dp

---
+++ src/leetcode/dp/组合总和4.java
@@ -0,0 +1,37 @@
+package leetcode.dp;
+
+import java.util.Arrays;
+
+public class 组合总和4 {
+    public static int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int[] f = new int[target + 1];
+        f[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < n && nums[j] <= i; j++) {
+                f[i] += f[i - nums[j]];
+            }
+        }
+        return f[target];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3};
+        System.out.println(combinationSum4(arr, 4));
+    }
+    private int dfs(int[] nums, int target) {
+        if (target == 0) {
+            return 1;
+        }
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > target) {
+                break;
+            }
+            ans += dfs(nums, target - nums[i]);
+        }
+        return ans;
+    }
+}
---
+++ src/leetcode/dp/统计放置房子的方式数.java
@@ -0,0 +1,25 @@
+package leetcode.dp;
+
+public class 统计放置房子的方式数 {
+    private final static int MOD = (int) (1e9 + 7);
+    public int countHousePlacements(int n) {
+        long[] f = new long[n + 1];
+        f[0] = 1;
+        f[1] = 2;
+        for (int i = 2; i <= n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        f[n] %= MOD;
+        return (int) (f[n] * f[n] % MOD);
+    }
+
+    private long dfs(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 2;
+        }
+        return dfs(n - 1) + dfs(n - 2);
+    }
+}
