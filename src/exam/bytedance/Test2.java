72acd35b0e89fc868f2f5003e20c74b1e6699624
付十一 <694256602@qq.com>
2024-05-19 19:48:28 +0800

    2024.5.19

---
+++ src/exam/bytedance/Test2.java
@@ -0,0 +1,34 @@
+package exam.bytedance;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        long[] coins = new long[n];
+        long[] params = new long[m];
+        for (int i = 0; i < n; i++) {
+            coins[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            params[i] = scanner.nextInt();
+        }
+        BigInteger result = calculateReturn(coins, params);
+        System.out.println(result);
+    }
+    static BigInteger calculateReturn(long[] coins, long[] params) {
+        BigInteger result = new BigInteger(String.valueOf(0));
+        for (long coin : coins) {
+            BigInteger term = new BigInteger(String.valueOf(1));
+            for (long p : params) {
+//                term *= (p - coin);
+                term.multiply(new BigInteger(String.valueOf(p - coin)));
+            }
+            result.add(term);
+        }
+        return result;
+    }
+}
